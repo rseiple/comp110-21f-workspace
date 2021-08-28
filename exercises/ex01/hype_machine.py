884be1d017b9ea54a00b4cfb7b4277fbeb1ba285
Riley Seiple <rseiple@unc.edu>
2021-08-28 14:58:30 -0400

    EX01 Assignment

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,11 @@
 # TODO: Write docstring here
+"""Program to hype a person up - name input required. If only this worked on me."""
 
 # TODO: Initialize __author__ variable here
+__author__ = 730524618
 
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("Right, gotcha " + name + "! You've got this!")
+print(name + ", whatever you've got to do today, you're gonna do it, and do it well.")
+print("Best of luck, " + name + ".")
---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,11 @@
+"""Exercise in numeric operators and converting types."""
+
+__author__ = 730524618
+
+left_num = int(input("Please enter a number for the left side: "))
+right_num = int(input("Please enter a number for the right side: "))
+
+print(str(left_num) + " ** " + str(right_num) + " is " + str((left_num) ** (right_num)))
+print(str(left_num) + " / " + str(right_num) + " is " + str((left_num) / (right_num)))
+print(str(left_num) + " // " + str(right_num) + " is " + str((left_num) // (right_num)))
+print(str(left_num) + " % " + str(right_num) + " is " + str((left_num) % (right_num)))
